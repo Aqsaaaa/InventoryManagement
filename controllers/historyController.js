1ba15018abf91a53902fc110a971172ab9a6333a
rey <reynaldigazali@gmai.com>
2025-02-18 00:31:28 +0800

    Fix ():perbaikan post form data history

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -1,5 +1,6 @@
 const db = require("../db");
-
+const multer = require("multer");
+const upload = multer();
 exports.getHistory = (req, res) => {
   db.query("SELECT * FROM riwayat_pemakaian", (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
@@ -8,15 +9,29 @@ exports.getHistory = (req, res) => {
 };
 
 exports.addHistory = (req, res) => {
-  const { id_barang, status, deskripsi_pemakaian } = req.body;
+  upload.none()(req, res, async function () {
+    const { id_barang, status, deskripsi_pemakaian } = req.body;
 
-  if (!id_barang || !status) {
-    return res.status(400).json({ message: "ID barang dan status wajib diisi" });
-  }
+    if (!id_barang || !status) {
+      return res
+        .status(400)
+        .json({ message: "ID barang dan status wajib diisi" });
+    }
 
-  db.query("INSERT INTO riwayat_pemakaian (id_barang, status, deskripsi_pemakaian) VALUES (?, ?, ?)",
-    [id_barang, status, deskripsi_pemakaian], (err, results) => {
-      if (err) return res.status(500).json({ error: err.message });
-      res.status(201).json({ id: results.insertId, id_barang, status, deskripsi_pemakaian });
-    });
+    db.query(
+      "INSERT INTO riwayat_pemakaian (id_barang, status, deskripsi_pemakaian) VALUES (?, ?, ?)",
+      [id_barang, status, deskripsi_pemakaian],
+      (err, results) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res
+          .status(201)
+          .json({
+            id: results.insertId,
+            id_barang,
+            status,
+            deskripsi_pemakaian,
+          });
+      }
+    );
+  });
 };
